d30e5bdd4c93da0b8d65c9b2aad24c0ad1af2d86
Ryan <81394855+cloudthepain@users.noreply.github.com>
2025-04-02 11:24:01 -0400

    Silencing errors and output in terminal

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
     let ip_addr = getuserinput();
 
     ping(&ip_addr);
+    attempt_to_ssh(&ip_addr);
 }
 
 fn getuserinput() -> String
@@ -36,6 +37,7 @@ fn ping(ip_address_to_ping: &str)
     let x = "-c 4";
     let status = Command::new("ping")
             .args([ip_address_to_ping, x])
+            .stdout(std::process::Stdio::null())
             .spawn()
             .expect("failed")
             .wait()
@@ -51,11 +53,34 @@ fn get_terminal_controllers()
 {
     let status = Command::new("bash")
     .arg("--version")
+    .stdout(std::process::Stdio::null())
     .status()
     .expect("Failed to locate bash");
 
     if status.success()
     {
         println!("Bash present");
+    }
+}
+
+fn attempt_to_ssh(ip_addr: &str)
+{
+    let ssh_user_name = "username";
+    let status = Command::new("ssh")
+            .args([ssh_user_name, ip_addr])
+            .stdout(std::process::Stdio::null())
+            .stderr(std::process::Stdio::null())
+            .spawn()
+            .expect("failed")
+            .wait()
+            .expect("failed during wait");
+
+    if status.success()
+    {
+        println!("SSH failed to connect")
+    }
+    if !status.success()
+    {
+        println!("You aren't able to run from this battle!")
     }
 }

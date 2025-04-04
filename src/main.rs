6d7f3cf69d9e837f5ed94763b3435959287ba9d2
Ryan <81394855+cloudthepain@users.noreply.github.com>
2025-04-04 11:53:49 -0400

    updated the menus to present cleaner.

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,27 @@ use std::io;
 
 fn main() {
 
-    const HOST_OS: &str = std::env::consts::OS;
+    createstartmenu();
+}
 
-    println!("{}",HOST_OS);
+fn createstartmenu()
+{
+    println!("What phase are you in?");
+    println!("1: Recon");
+    println!("2: Not Recon");
+    println!("3: Third Option");
+    println!("Option: ");
 
-
-    get_terminal_controllers();
-
-
-    let ip_addr = getuserinput();
-
-    ping(&ip_addr);
-    attempt_to_ssh(&ip_addr);
+    let value = getuserinput();
+    let num:i32 = value.trim().parse().expect("Failed to parse int");
+    if num == 1
+    {
+        ping();
+    }
+    else if num == 2
+    {
+        attempt_to_ssh();
+    }
 }
 
 fn getuserinput() -> String
@@ -31,9 +40,11 @@ fn getuserinput() -> String
     return trim_value;
 }
 
-fn ping(ip_address_to_ping: &str)
+fn ping()
 {
 
+    println!("Enter IP to Recon: ");
+    let ip_address_to_ping = &getuserinput();
     let x = "-c 4";
     let status = Command::new("ping")
             .args([ip_address_to_ping, x])
@@ -45,7 +56,14 @@ fn ping(ip_address_to_ping: &str)
 
     if status.success()
     {
-        println!("Completed Successfully Pokemon Master")
+
+        println!("Ping Completed Successfully");
+        std::process::exit(status.code().unwrap());
+    }
+    else
+    {
+        println!("Could not successfully ping!");
+        std::process::exit(status.code().unwrap());
     }
 }
 
@@ -63,8 +81,16 @@ fn get_terminal_controllers()
     }
 }
 
-fn attempt_to_ssh(ip_addr: &str)
+fn attempt_to_ssh()
 {
+
+    print!("Enter ip to SSH into: ");
+    let y = getuserinput();
+
+
+    let ip_addr = y.trim();
+    
+    
     let ssh_user_name = "username";
     let status = Command::new("ssh")
             .args([ssh_user_name, ip_addr])
